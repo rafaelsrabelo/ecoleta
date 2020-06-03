45cecf1e0bd34fe7b4cdcd4f002e27c19063e987
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2020-06-03 13:47:20 -0300

    Correção no Formulario de ponto de coleta

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,44 +1,44 @@
-function populateUfs() {
-  const ufSelect = document.querySelector("select[name=uf]")
-  const stateInput = document.querySelector("input[name=state]")
 
-  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-  .then( res => res.json() )
+
+function populateUFs() {
+  const ufSelect = document.querySelector("select[name=uf]")
+
+  fetch("https:servicodados.ibge.gov.br/api/v1/localidades/estados")
+  .then( res =>  res.json() )
   .then( states => {
 
-    for( const state of states ) {
-      ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-    }
-
+      for( const state of states) {
+          ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+      }
   } )
 }
 
-populateUfs()
+populateUFs()
+
 
 function getCities(event) {
-  const  citySelect = document.querySelector("select[name=city]")
-
-  const ufValue = event.target.value
+  const citySelect = document.querySelector("select[name=city]")
+  const stateSelect = document.querySelector("input[name=state]")
 
   const indexOfSelectedState = event.target.selectedIndex
-  stateInput.value = event.target.options[indexOfSelectedState].text
-  
+  stateSelect.value = event.target.options[indexOfSelectedState].text
+
+  const ufValue = event.target.value
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
   fetch(url)
-  .then( res => res.json() )
+  .then( res =>  res.json() )
   .then( cities => {
 
-    for( const city of cities ) {
-      ufSelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-    }
+      for( const city of cities) {
+          citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      }
 
       citySelect.disabled = false
   } )
 }
 
- document
+document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities)
-
