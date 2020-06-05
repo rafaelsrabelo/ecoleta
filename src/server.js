67e7d1d8ab867fe7f6009749549603e0575ea4ac
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2020-06-05 13:53:19 -0300

    Terminando o backend

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const server = express()
 
+//pegar o banco de dados
+const db = require("./database/db")
+
 
 //configurar pasta publica
 server.use(express.static("public"))
@@ -23,11 +26,34 @@ server.get("/", (req, res) => {
 })
 
 server.get("/create-point", (req, res) => {
+
+  // req.query: Query.Strings da nossa url
+  console.log(req.query)
+
   return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+  return res.send("ok")
+})
+
 server.get("/search", (req, res) => {
-  return res.render("search-results.html")
+
+  // pegar os dados do banco de dados
+    db.all(`SELECT * FROM places`, function (err, rows) {
+      if (err) {
+        return console.log(err)
+      }
+
+      const total = rows.length
+
+      console.log("Aqui estão seus registros!")
+      console.log(rows)
+
+      // mostrar a pagian html com os dados do db
+      return res.render("search-results.html", { places: rows, total: total})
+
+    })
 })
 
 // ligar o servidor 
