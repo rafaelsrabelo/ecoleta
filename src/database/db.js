82e0c39f8a54a18e1a61cd8d93f8e7fd46894925
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2020-06-06 19:30:11 -0300

    Configurando database

--- src/database/db.js
+++ src/database/db.js
@@ -68,7 +68,7 @@ module.exports = db
   // })
 
   // 4) deletar um dado da tabela
-  db.run(`DELETE FROM places WHERE id = ?`, [3], function (err) {
+  db.run(`DELETE FROM places WHERE id = ?`, [6], function (err) {
     if (err) {
       return console.log(err)
     }
--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ const db = require("./database/db")
 //configurar pasta publica
 server.use(express.static("public"))
 
+// habilitar o uso do req.body na nossa app
+server.use(express.urlencoded({ extended: true }))
 
 // utilizando template engine 
 const nunjuscks = require("nunjucks")
@@ -28,19 +30,61 @@ server.get("/", (req, res) => {
 server.get("/create-point", (req, res) => {
 
   // req.query: Query.Strings da nossa url
-  console.log(req.query)
+  //console.log(req.query)
 
   return res.render("create-point.html")
 })
 
 server.post("/savepoint", (req, res) => {
-  return res.send("ok")
+
+// console.log(req.body)
+
+  // inserir dados no banco de dados 
+  const query = `
+    INSERT INTO places (
+      image, 
+      name, 
+      address, 
+      address2, 
+      state, 
+      city, 
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+  `
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+  function afterInsertData(err) {
+    if (err) {
+      return console.log(err)
+    }
+
+    console.log("Cadastrado com sucesso!")
+    console.log(this)
+  }
+
+  db.run(query, values, afterInsertData )
+
+  return res.render("create-point.html", {saved: true})
 })
+
 
 server.get("/search", (req, res) => {
 
+  const search = req.query.search
+
+    if(search == "") {
+      return res.render("search-results.html", { total: 0})
+    }
+
   // pegar os dados do banco de dados
-    db.all(`SELECT * FROM places`, function (err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
       if (err) {
         return console.log(err)
       }
